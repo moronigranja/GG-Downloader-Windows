c62c297ba1f39cfa1805517cdae9c448dd21dd0a
Moroni Granja <m.oliveira.granja@avanade.com>
2023-01-24 16:54:19 -0300

    Added retry policy for HttpRequestProvider

--- gg-downloader/Services/HttpRequestProvider.cs
+++ gg-downloader/Services/HttpRequestProvider.cs
@@ -2,20 +2,39 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Polly;
+using Polly.Retry;
 
 namespace gg_downloader.Services
 {
     internal class HttpRequest
     {
 
-        static readonly HttpClient client = new HttpClient();
+        static readonly HttpClient client;
+        static readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
 
-        public static async Task< HttpResponseMessage> Get(Uri uri)
+        static HttpRequest()
+        {
+            client = new HttpClient();
+            _retryPolicy = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                     .Or<Exception>()
+                     .RetryAsync(10, onRetry: (delegateResult, retryCount) =>
+                            {
+                                Console.Out.WriteLine($"Error: {delegateResult?.Exception?.Message ?? "No exception"}, retrying: {retryCount}/10");
+                                Thread.Sleep(500);
+                            });
+        }
+
+        public static async Task<HttpResponseMessage> Get(Uri uri)
         {
             client.DefaultRequestHeaders.UserAgent.ParseAdd("GOG-Downloader-Win/1.0");
+            
+            HttpResponseMessage response = await _retryPolicy.ExecuteAsync(async () =>
+                await client.GetAsync(uri)
+            );
 
-            HttpResponseMessage response = await client.GetAsync(uri);
             return response;
         }
 
@@ -26,7 +45,10 @@ namespace gg_downloader.Services
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
             client.DefaultRequestHeaders.UserAgent.ParseAdd("GOG-Downloader-Win/1.0");
 
-            HttpResponseMessage response = await client.GetAsync(uri);
+            HttpResponseMessage response = await _retryPolicy.ExecuteAsync(async () =>
+                await client.GetAsync(uri)
+            );
+
             return response;
         }
 
